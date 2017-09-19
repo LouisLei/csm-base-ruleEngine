89208cd65ea5ef2adb0c03e914c8714c14c39db3
yinwenjie <yinwenjie@vanda->
2017-09-19 11:30:19 +0800

    完成了策略下批量rule的执行功能开发，并完成了大致测试

--- com.cheshangma.platform.ruleEngine.core/src/main/java/com/cheshangma/platform/ruleEngine/core/executor/ScriptCaller.java
+++ com.cheshangma.platform.ruleEngine.core/src/main/java/com/cheshangma/platform/ruleEngine/core/executor/ScriptCaller.java
@@ -1,6 +1,8 @@
 package com.cheshangma.platform.ruleEngine.core.executor;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.Callable;
@@ -17,6 +19,8 @@ import javax.script.ScriptException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.cheshangma.platform.ruleEngine.core.utils.DeepCopyUtils;
+import com.cheshangma.platform.ruleEngine.enums.ExecModeType;
 import com.cheshangma.platform.ruleEngine.enums.ResultModeType;
 import com.google.common.cache.Cache;
 import com.google.common.cache.CacheBuilder;
@@ -25,12 +29,13 @@ import com.google.common.cache.CacheBuilder;
  * 这是专门用于进行脚本代码执行的运行器。
  * @author yinwenjie
  */
-public class ScriptCaller implements Callable<ScriptCaller.Result> {
+public class ScriptCaller implements Callable<List<ScriptCaller.Result>> {
   
   /**
-   * 需要执行的groovy脚本在这里
+   * 需要执行的groovy脚本在这里<br>
+   * 可能是一个脚本，也可能是多个脚本
    */
-  private String groovyExpression;
+  private List<String> groovyExpressions;
   
   /**
    * 绑定的执行参数
@@ -46,6 +51,11 @@ public class ScriptCaller implements Callable<ScriptCaller.Result> {
    * 这里是一个本地缓存，基于google的一个本地缓存实现。
    */
   private static Cache<Object,Object> CACHE = null;
+  
+  /**
+   * 批量脚本的执行要求
+   */
+  private ExecModeType execModeType;
   
   /**
    * 日志
@@ -67,14 +77,58 @@ public class ScriptCaller implements Callable<ScriptCaller.Result> {
         .build();
   }
   
-  public ScriptCaller(String groovyExpression , Map<String, Object> variables , boolean allowInverse) {
-    this.groovyExpression = groovyExpression;
+  public ScriptCaller(List<String> groovyExpressions , Map<String, Object> variables , boolean allowInverse , ExecModeType execModeType) {
+    this.groovyExpressions = groovyExpressions;
     this.variables = variables;
     this.allowInverse = allowInverse;
+    this.execModeType = execModeType;
   }
   
   @Override
-  public Result call() {
+  public List<Result> call() {
+    // 这是当前执行线程中的上下文信息
+    ScriptThread scriptThread = (ScriptThread)Thread.currentThread();
+    ExecuteContext context = scriptThread.getExecuteContext();
+    // 多次执行的结果在这里放置
+    List<Result> allResult = new ArrayList<>();
+    // 初始化的入参信息
+    Map<String, Object> currentVariables = variables;
+    
+    for (int index = 0 ; index < groovyExpressions.size() ; index++) {
+      String groovyExpression = groovyExpressions.get(index);
+      Result result = this.executeScript(groovyExpression, currentVariables, this.allowInverse, context);
+      allResult.add(result);
+      
+      // 根据执行结果和设置情况，决定是否继续运行
+      // 如果条件成立，说明执行没有成功，且设置执行方式为“一旦执行失败，就停止执行”
+      if(result.getResultMode() != ResultModeType.PASS
+          && this.execModeType == ExecModeType.SIMPLE) {
+        break;
+      }
+      
+      // 重新构造入参，上一个脚本的执行结果就是下次脚本执行的入参（能不做深度复制就不做深度复制）
+      Map<String, Object> outputs = result.getScore();
+      if(outputs != null && !outputs.isEmpty() && groovyExpressions.size() > 1) {
+        // 深度复制——为了不影响之前的输出结果
+        currentVariables = DeepCopyUtils.deepCopy(outputs);
+      } else {
+        currentVariables = new HashMap<>();
+      }
+    }
+    
+    // 返回多有脚本的执行结果
+    return allResult;
+  }
+  
+  /**
+   * 该私有方法用于执行某一单次脚本
+   * @param groovyExpression
+   * @param variables
+   * @param allowInverse 
+   * @param context 本次可能由外部传入的上下文对象，也可能没有
+   * @return
+   */
+  private Result executeScript(String groovyExpression , Map<String, Object> variables , boolean allowInverse , ExecuteContext context) {
     /*
      * 执行过程为：
      * 1、首先确定当前执行的内容是否需要支持反调。如果需要支持反调则需要重新组合groovy脚本
@@ -83,10 +137,10 @@ public class ScriptCaller implements Callable<ScriptCaller.Result> {
      * 4、开始构造返回信息，返回信息不只包括脚本的正式返回值，还包括了bings中的信息（而且主要还是后者）
      * */
     // 如果条件成立，说明需要支持反调（组合的javaProcess方法不一样）
-    if(this.allowInverse) {
-      this.groovyExpression = "def javaProcess(def componentAndMethod) {com.cheshangma.platform.ruleEngine.core.executor.ScriptInverseRunner inverseRunner = com.cheshangma.platform.ruleEngine.core.executor.ScriptInverseRunner.getNewInstance();inverseRunner.inverse(componentAndMethod);};" + this.groovyExpression;
+    if(allowInverse) {
+      groovyExpression = "def javaProcess(def componentAndMethod) {com.cheshangma.platform.ruleEngine.core.executor.ScriptInverseRunner inverseRunner = com.cheshangma.platform.ruleEngine.core.executor.ScriptInverseRunner.getNewInstance();inverseRunner.inverse(componentAndMethod);};" + groovyExpression;
     } else {
-      this.groovyExpression = ";def javaProcess(def componentAndMethod) {throw new com.cheshangma.platform.ruleEngine.core.exception.ScriptInverseRejectException(\"remote execute process not allow inverse!\");};" + this.groovyExpression;
+      groovyExpression = "def javaProcess(def componentAndMethod) {throw new com.cheshangma.platform.ruleEngine.core.exception.ScriptInverseRejectException(\"remote execute process not allow inverse!\");};" + groovyExpression;
     }
     
     // 2、=============试图从本地缓存中找到脚本
@@ -94,7 +148,7 @@ public class ScriptCaller implements Callable<ScriptCaller.Result> {
     ScriptEngine engine = manager.getEngineByName("groovy");
     CompiledScript compiledScript = null;
     try {
-      Object cacheObject = CACHE.get(this.groovyExpression.hashCode(), new CompiledScriptCreator(this.groovyExpression));
+      Object cacheObject = CACHE.get(groovyExpression.hashCode(), new CompiledScriptCreator(groovyExpression));
       if(cacheObject != null) {
         compiledScript = (CompiledScript)cacheObject;
       }
@@ -104,26 +158,25 @@ public class ScriptCaller implements Callable<ScriptCaller.Result> {
     }
     
     // 绑定外部变量
+    Result result = new Result();
     Bindings binding = engine.createBindings();
-    if(this.variables != null && !this.variables.isEmpty()) {
-      Set<String> keys = this.variables.keySet();
+    if(variables != null && !variables.isEmpty()) {
+      // 深度复制后最为一个副本保存起来
+      // 因为在进入脚本执行后，这些对象的属性可能就会改变了
+      result.setInput(DeepCopyUtils.deepCopy(variables));
+      Set<String> keys = variables.keySet();
       for (String key : keys) {
-        binding.put(key, this.variables.get(key));
+        binding.put(key, variables.get(key));
       }
     }
     // 绑定上下文信息
-    ScriptThread scriptThread = (ScriptThread)Thread.currentThread();
-    ExecuteContext context = scriptThread.getExecuteContext();
     binding.put("context", context);
     
     // 3、=============开始执行
-    Result result = new Result();
-    Object returnResult = null;
     try {
-      returnResult = compiledScript.eval(binding);
+      compiledScript.eval(binding);
       result.resultMode = ResultModeType.PASS;
       result.score = new HashMap<>();
-      result.score.put("_return", returnResult);
     } catch (ScriptException e) {
       LOG.error(e.getMessage() , e);
       result.rejectMessage = e.getMessage();
@@ -179,6 +232,10 @@ public class ScriptCaller implements Callable<ScriptCaller.Result> {
      */
     private Map<String, Object> score;
     /**
+     * 记录当前运行脚本的输入参数
+     */
+    private Map<String, Object> input;
+    /**
      * 被拒绝后，或者执行失败后显示的信息
      */
     private String rejectMessage = "";
@@ -199,6 +256,12 @@ public class ScriptCaller implements Callable<ScriptCaller.Result> {
     }
     public void setRejectMessage(String rejectMessage) {
       this.rejectMessage = rejectMessage;
+    }
+    public Map<String, Object> getInput() {
+      return input;
+    }
+    public void setInput(Map<String, Object> input) {
+      this.input = input;
     }
   }
 }
--- com.cheshangma.platform.ruleEngine.core/src/main/java/com/cheshangma/platform/ruleEngine/core/framework/SimpleRuleEngineFramework.java
+++ com.cheshangma.platform.ruleEngine.core/src/main/java/com/cheshangma/platform/ruleEngine/core/framework/SimpleRuleEngineFramework.java
@@ -1,5 +1,7 @@
 package com.cheshangma.platform.ruleEngine.core.framework;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -23,6 +25,7 @@ import com.cheshangma.platform.ruleEngine.core.service.PolicyService;
 import com.cheshangma.platform.ruleEngine.core.service.RuleService;
 import com.cheshangma.platform.ruleEngine.core.service.ServiceAbstractFactory;
 import com.cheshangma.platform.ruleEngine.core.utils.DeepCopyUtils;
+import com.cheshangma.platform.ruleEngine.enums.ExecModeType;
 import com.cheshangma.platform.ruleEngine.enums.PolicyModeType;
 import com.cheshangma.platform.ruleEngine.enums.ResultModeType;
 import com.cheshangma.platform.ruleEngine.core.executor.ScriptThreadFactory;
@@ -89,17 +92,16 @@ class SimpleRuleEngineFramework implements RuleEngineFramework {
     executionResult.setDescription(rule.getDescription());
     
     //2、========= 构造输入（如果不是基础类型就需要做深度复制）
-    // 因为在进入脚本执行后，这些对象的属性可能就会改变了
-    Map<String, Object> copyInputs = DeepCopyUtils.deepCopy(inputs);
-    executionResult.setInputs(copyInputs);
-    Future<Result> future = this.executeSript(expression , inputs);
+    Future<List<Result>> future = this.executeScript(Arrays.asList(new String[]{expression}) , inputs , ExecModeType.PASSBY);
     
     // 3、================
     ScriptCaller.Result result = null;
     try {
-      result = future.get();
+      // 只执行一个单一脚本，就这样可以返回了
+      result = future.get().get(0);
       executionResult.setRejectMessage(result.getRejectMessage());
       executionResult.setResultMode(result.getResultMode());
+      executionResult.setInputs(result.getInput());
       executionResult.setScore(result.getScore());
     } catch (InterruptedException | ExecutionException e) {
       LOG.error(e.getMessage() , e);
@@ -123,55 +125,50 @@ class SimpleRuleEngineFramework implements RuleEngineFramework {
     executionResult.setCreated(new Date());
     executionResult.setCreator(policy.getCreator());
     executionResult.setDescription(policy.getDescription());
+    executionResult.setId(UUID.randomUUID().toString());
+    executionResult.setPolicyId(policy.getId());
     
     // 2、==============
+    // 记录输入值副本
     Map<String, Object> copyInputs = DeepCopyUtils.deepCopy(inputs);
-    Future<Result> future = null;
+    Future<List<Result>> futures = null;
     executionResult.setInputs(copyInputs);
     // 如果条件成立，说明policy策略中已带有执行信息，直接执行即可
     if(policy.getMode() == PolicyModeType.RULEMODE_SIMPLE) {
       String expression = policy.getScoreExpression();
       Validate.notBlank(expression , "expression not be empty!! ");
-      future = this.executeSript(expression , inputs);
+      futures = this.executeScript(Arrays.asList(new String[]{expression}) , inputs , ExecModeType.PASSBY);
+      return this.buildSimplePolicyResult(futures, executionResult, policy);
     } 
     // 否则就是批量执行其中的rule规则
     else if (rules != null && !rules.isEmpty()) {
-      // TODO 这里还没有写,多步执行
+      List<String> expressions = new ArrayList<>();
+      for (RuleModel rule : rules) {
+        String expression = rule.getExpression();
+        Validate.notBlank(expression , "expression not be empty!! ");
+        expressions.add(expression);
+      }
+      futures = this.executeScript(expressions, copyInputs, policy.getExecMode());
+      return this.buildMultiplePolicyResult(futures , rules , executionResult, policy);
     } else {
       executionResult.setRejectMessage("rules is not be empty!!");
       executionResult.setResultMode(ResultModeType.EXCEPTION);
       return executionResult;
     }
-    executionResult.setId(UUID.randomUUID().toString());
-    executionResult.setPolicyId(policy.getId());
     
-    // 3、================
-    ScriptCaller.Result result = null;
+  }
+  
+  /**
+   * 该私有方法用于在批量执行一个policy下的多个脚本后，构造返回信息
+   * @param futures
+   * @param executionResult
+   * @param policy
+   * @return
+   */
+  private ExecutionPolicyModel buildMultiplePolicyResult(Future<List<Result>> futures , List<RuleModel> rules , ExecutionPolicyModel executionResult , PolicyModel policy) {
+    List<Result> results = null;
     try {
-      result = future.get();
-      Map<String, Object> score = result.getScore();
-      executionResult.setScore(result.getScore());
-      MetadataModel metadata = policy.getMetadata();
-      // 确定元数据
-      if(metadata != null) {
-        Set<VariableProperty> targetVars = metadata.getParams();
-        MetadataModel existMetadata = new MetadataModel();
-        Set<VariableProperty> existVars = new HashSet<VariableProperty>();
-        existMetadata.setParams(existVars);
-        
-        // 只有有值才能记录到结果中
-        if(targetVars != null && !targetVars.isEmpty()) {
-          for (VariableProperty variable : targetVars) {
-             String variableName = variable.getName();
-             Object value = score.get(variableName);
-             if(value != null) {
-               variable.setValue(value);
-               existVars.add(variable);
-             }
-          }
-        }
-        executionResult.setMetadata(existMetadata);
-      }
+      results = futures.get();
     } catch (InterruptedException | ExecutionException e) {
       LOG.error(e.getMessage() , e);
       executionResult.setRejectMessage(e.getMessage());
@@ -179,20 +176,121 @@ class SimpleRuleEngineFramework implements RuleEngineFramework {
       return executionResult;
     }
     
-    // 4、================
-    executionResult.setRejectMessage(result.getRejectMessage());
-    executionResult.setResultMode(result.getResultMode());
+    // 3、================
+    List<ExecutionRuleModel> ruleResults = new ArrayList<>();
+    // 构造每一个rule的执行结果
+    for (int index = 0 ; results != null && index < results.size() ; index++) {
+      RuleModel ruleModel = rules.get(index);
+      ScriptCaller.Result result = results.get(index);
+      ExecutionRuleModel ruleResult = new ExecutionRuleModel();
+      ruleResult.setCreated(new Date());
+      ruleResult.setDescription(ruleModel.getDescription());
+      ruleResult.setId(UUID.randomUUID().toString());
+      ruleResult.setRuleId(ruleModel.getRuleId());
+      ruleResult.setInputs(result.getInput());
+      ruleResult.setScore(result.getScore());
+      
+      ruleResult.setResultMode(result.getResultMode());
+      // 如果条件成立，说明执行成功
+      if(result.getResultMode() == ResultModeType.PASS) {
+        ruleResult.setRejectMessage("");
+      } else {
+        ruleResult.setRejectMessage(result.getRejectMessage());
+      }
+      ruleResults.add(ruleResult);
+    }
+    executionResult.setRuleResults(ruleResults);
+      
+    // 4、============ 确定元数据
+    ScriptCaller.Result lastResult = results.get(results.size() - 1);
+    MetadataModel metadata = policy.getMetadata();
+    Map<String, Object> lastScore = lastResult.getScore();
+    if(metadata != null) {
+      Set<VariableProperty> targetVars = metadata.getParams();
+      MetadataModel existMetadata = new MetadataModel();
+      Set<VariableProperty> existVars = new HashSet<VariableProperty>();
+      existMetadata.setParams(existVars);
+      
+      // 只有有值才能记录到结果中
+      if(targetVars != null && !targetVars.isEmpty()) {
+        for (VariableProperty variable : targetVars) {
+           String variableName = variable.getName();
+           Object value = lastScore.get(variableName);
+           if(value != null) {
+             variable.setValue(value);
+             existVars.add(variable);
+           }
+        }
+      }
+      executionResult.setMetadata(existMetadata);
+    }
+    
+    // 使用最后一个rule的输出，作为整个policy的输出
+    executionResult.setScore(lastScore);
+    executionResult.setResultMode(lastResult.getResultMode());
+    // 可能的错误
+    executionResult.setRejectMessage(lastResult.getRejectMessage());
     return executionResult;
   }
   
   /**
-   * 该私有方法用于执行脚本
+   * 该私有方法用于在执行单个policy策略脚本后，构造返回信息
+   * @param futures
+   * @param executionResult
+   * @param policy
    * @return
    */
-  private Future<Result> executeSript(String expression, Map<String, Object> inputs) {
-    ScriptCaller scriptCaller = new ScriptCaller(expression, inputs , this.allowInverse);
-    Future<Result> future = this.scriptThreadPoolExecutor.submit(scriptCaller);
-    return future;
+  private ExecutionPolicyModel buildSimplePolicyResult(Future<List<Result>> futures , ExecutionPolicyModel executionResult , PolicyModel policy) {
+    List<Result> results = null;
+    try {
+      results = futures.get();
+    } catch (InterruptedException | ExecutionException e) {
+      LOG.error(e.getMessage() , e);
+      executionResult.setRejectMessage(e.getMessage());
+      executionResult.setResultMode(ResultModeType.EXCEPTION);
+      return executionResult;
+    }
+    
+    // 3、================
+    ScriptCaller.Result result = results.get(0);
+    Map<String, Object> score = result.getScore();
+    executionResult.setScore(result.getScore());
+    MetadataModel metadata = policy.getMetadata();
+    
+    // 4、================ 确定元数据
+    if(metadata != null) {
+      Set<VariableProperty> targetVars = metadata.getParams();
+      MetadataModel existMetadata = new MetadataModel();
+      Set<VariableProperty> existVars = new HashSet<VariableProperty>();
+      existMetadata.setParams(existVars);
+      
+      // 只有有值才能记录到结果中
+      if(targetVars != null && !targetVars.isEmpty()) {
+        for (VariableProperty variable : targetVars) {
+           String variableName = variable.getName();
+           Object value = score.get(variableName);
+           if(value != null) {
+             variable.setValue(value);
+             existVars.add(variable);
+           }
+        }
+      }
+      executionResult.setMetadata(existMetadata);
+    }
+    executionResult.setResultMode(result.getResultMode());
+    // 可能的错误信息
+    executionResult.setRejectMessage(result.getRejectMessage());
+    return executionResult;
+  }
+  
+  /**
+   * 该私有方法用于执行脚本，支持一个或者多个脚本依次执行
+   * @return
+   */
+  private Future<List<Result>> executeScript(List<String> expressions, Map<String, Object> inputs , ExecModeType execMode) {
+    ScriptCaller scriptCaller = new ScriptCaller(expressions, inputs , this.allowInverse , execMode);
+    Future<List<Result>> futures = this.scriptThreadPoolExecutor.submit(scriptCaller);
+    return futures;
   }
 
   /* (non-Javadoc)
